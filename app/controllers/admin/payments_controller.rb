d6a81a9e341245a786b737826955b6b1975584f9
Michael de Silva <michael@mwdesilva.com>
2011-10-01 21:32:32 +0300

    clean up payments_controller

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -7,9 +7,7 @@ class Admin::PaymentsController < ApplicationController
   
   def create
     @partner = OfflinePartner.find(params[:offline_partner_id])
-    @payment = @partner.payments.new(params[:payment])  
-    # @payment.barcode_identifier = params[:payment][:barcode_identifer]
-    # @payment = @partner.payments.new(params[:payment])  
+    @payment = @partner.payments.new(params[:payment])   
   
     if @payment.save
       flash[:notice] = "Successfully made payment at #{@partner.name}."
